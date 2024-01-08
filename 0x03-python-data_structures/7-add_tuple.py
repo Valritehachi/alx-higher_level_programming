fe307e6bf03303d890dfc1908e72c50b7af1aa7d
Valritehachi <valritehachi@gmail.com>
2024-01-07 19:45:22 -0800

    edits

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple_a is None or len(tuple_a) == 0:
+        tuple_a = (0, 0)
+    if tuple_b is None or len(tuple_b) == 0:
+        tuple_b = (0, 0)
+    if len(tuple_a) == 1:
+        tuple_a = (tuple_a[0], 0)
+    if len(tuple_b) == 1:
+        tuple_b = (tuple_b[0], 0)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
