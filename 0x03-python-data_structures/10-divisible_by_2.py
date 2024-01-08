f3ed675a57fa670ff57e15e88b838768671e78c0
Valritehachi <valritehachi@gmail.com>
2024-01-07 21:11:13 -0800

    updates

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 def divisible_by_2(my_list=[]):
     if my_list is None:
-        return None
+        return(None)
+
     new_list = []
     for i in range(len(my_list)):
         new_list.append(my_list[i] % 2 == 0)
+
     return(new_list)
