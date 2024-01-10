2b34a0244bee0249bf15d618e0500f330dadb1d1
Valritehachi <valritehachi@gmail.com>
2024-01-10 02:59:27 -0800

    edits

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
+def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    new_dict = {}
-    for key in list(a_dictionary):
-        new_dict[key] = a_dictionary[key] * 2
-    return new_dict
-
-#!/usr/bin/python3
-
-def best_score(a_dictionary):
-    if a_dictionary is None or not a_dictionary:
-        return None
-
-    max_key = max(a_dictionary, key=a_dictionary.get)
+    max_key = next(iter(a_dictionary))
+    for key in a_dictionary:
+        if a_dictionary[key] > a_dictionary[max_key]:
+            max_key = key
     return max_key
-
-# Example usage:
-my_dictionary = {"John": 90, "Alice": 85, "Bob": 95}
-result = best_score(my_dictionary)
-print(result)
-
