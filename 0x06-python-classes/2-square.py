80e37d56827a3e2d162a9de443b5d4e9ca8510e2
valritehachi <valritehachi@gmail.com>
2024-01-23 09:35:41 +0000

    edits

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,12 +3,11 @@
 """Defines a class."""
 
 class Square:
-    """shows a square."""
-
-    def __init__(self, size=0):
-        """starts up a new Shape."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+	"""shows a shape"""
+	def __init__(self, size=0):
+		"""starts up a shape"""
+		if not isinstance(size, int):
+			raise TypeError("size must be an integer")
+		elif size < 0:
+			raise ValueError("size must be >= 0")
+		self.__size = size
