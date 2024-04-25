76d28c4e3bbf1f3d8ac4a4572fe62bdbe1549e88
Valritehachi <valritehachi@gmail.com>
2024-04-25 05:45:25 -0700

    edits

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument.
+# sends a request to a URL passed as an argument to the code .
 awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
