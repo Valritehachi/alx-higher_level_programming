ebaf8d619a616b9135dc347474cdee64ca3612ce
Valritehachi <valritehachi@gmail.com>
2024-04-25 21:49:46 -0700

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This script does the folowing
+- fetching of a url.
+- displays the body of the reference
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
